b00c318085d8e24bff373ae00b3e2a8e13083830
Mohabake <doctormoha6718@gmail.com>
2018-03-06 04:37:00 +0300

    modified javascript code

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDJEPpuLJPj-1NcwECuu8MvecJbaEv7zSM",
+  authDomain: "login-page-f34ba.firebaseapp.com",
+  databaseURL: "https://login-page-f34ba.firebaseio.com",
+  projectId: "login-page-f34ba",
+  storageBucket: "login-page-f34ba.appspot.com",
+  messagingSenderId: "1079513476472"
+};
+firebase.initializeApp(config);
