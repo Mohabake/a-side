c56b8ed4f5861f7472c8481cef345a5699f00d3b
Emmanuel Mutuku <manueldizzy@gmail.com>
2018-03-08 12:38:01 +0300

    .

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+             $(".clickable").click(function() {
+               $("#nice-guy-showing").toggle();
+             });
+           });
+      $(document).ready(function() {
+              $(".clickable").click(function() {
+                $("#big-shot-showing").toggle();
+              });
+            });
+            $(document).ready(function() {
+                         $(".clickable").click(function() {
+                           $("#older-guy-showing").toggle();
+                         });
+                       });
+                  $(document).ready(function() {
+                          $(".clickable").click(function() {
+                            $("#man-man-showing").toggle();
+                          });
+                        });
+                        $(document).ready(function() {
+                                $(".clickable").click(function() {
+                                  $("#fun-social-showing").toggle();
+                                });
+                              });
